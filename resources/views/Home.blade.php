60a282a8b251923b139f2abc485d55fd125c292e
nourelhoudaas <sayahhouda06@gmail.com>
2024-08-25 12:18:48 +0100

    add

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -13,8 +13,12 @@
 
         <!--========== BOX ICONS ==========-->
        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+       <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" >
+       <link rel="stylesheet" href="https://cdn.datatables.net/2.1.4/css/dataTables.dataTables.min.css">
+
 
 
 
@@ -37,7 +41,11 @@
          {{-- nos script js--}}
 
 
-
+         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" ></script>
+         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" ></script>
+         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/js/bootstrap.min.js" ></script>
+         <script src="https://cdn.datatables.net/2.1.4/js/dataTables.min.js" ></script>
+         <script src="app.js"></script>
 
 
 </html>
